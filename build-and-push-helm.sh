dd7ace91b1e8f13f5a6c6eae0ed118331852d627
Roee Shachar <shacharroy142gmail.com>
2024-05-09 18:50:40 +0300

    feat: breaking

--- build-and-push-helm.sh
+++ build-and-push-helm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $WORKDIR
 helm registry login $ACR_URL --username $ACR_PUSH_USER --password $ACR_PUSH_TOKEN
 CHART_NAME=$(cat Chart.yaml | grep name | awk '{print $2; exit}')
 CHART_VERSION=$(cat Chart.yaml | grep version | awk '{print $2; exit}')
