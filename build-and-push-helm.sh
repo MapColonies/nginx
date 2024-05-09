605e3ce4f4f3cba01fe624416c3f183455704830
Roee Shachar <shacharroy142gmail.com>
2024-05-09 19:07:56 +0300

    feat: breaking

--- build-and-push-helm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $WORKDIR
-helm registry login $ACR_URL --username $ACR_PUSH_USER --password $ACR_PUSH_TOKEN
-CHART_NAME=$(cat Chart.yaml | grep name | awk '{print $2; exit}')
-CHART_VERSION=$(cat Chart.yaml | grep version | awk '{print $2; exit}')
-helm package --dependency-update .
-helm push $CHART_NAME-$CHART_VERSION.tgz oci://$ACR_URL/helm
